95833f5accb8214328b84d829741bee18787ef77
Piper Chester <piperchester@gmail.com>
2014-04-04 10:44:47 -0400

    Add some minor and 7 chords.

--- app/src/main/java/com/example/ukulelechords/app/dummy/DummyContent.java
+++ app/src/main/java/com/example/ukulelechords/app/dummy/DummyContent.java
@@ -26,17 +26,30 @@ public class DummyContent {
     static {
         // Using hex for some of the fingerings, leading 0 defaults to octal!
         addItem(new DummyItem("1", "A", 2220));
-        addItem(new DummyItem("2", "A#/Bb", 0xCEE));
-        addItem(new DummyItem("3", "B", 1402));
-        addItem(new DummyItem("4", "C", 2010));
-        addItem(new DummyItem("5", "C#/Db", 3120));
-        addItem(new DummyItem("6", "D", 0xE8));
-        addItem(new DummyItem("7", "D#/Eb", 1343));
-        addItem(new DummyItem("8", "E", 2100));
-        addItem(new DummyItem("9", "F", 3211));
-        addItem(new DummyItem("10", "F#/Gb", 4322));
-        addItem(new DummyItem("11", "G", 0x3));
-        addItem(new DummyItem("12", "G#/Ab", 1114));
+        addItem(new DummyItem("2", "Am", 2210));
+        addItem(new DummyItem("3", "A7", 2223));
+        addItem(new DummyItem("4", "A#/Bb", 0xCEE));
+        addItem(new DummyItem("5", "Bbm", 3321));
+        addItem(new DummyItem("6", "B", 1402));
+        addItem(new DummyItem("7", "Bm", 0x1B0));
+        addItem(new DummyItem("8", "B7", 1202));
+        addItem(new DummyItem("9", "C", 2010));
+        addItem(new DummyItem("10", "Cm", 1013));
+        addItem(new DummyItem("11", "C7", 2313));
+        addItem(new DummyItem("12", "C#/Db", 3120));
+        addItem(new DummyItem("13", "D", 0xE8));
+        addItem(new DummyItem("14", "Dm", 0xE7)); // 0231
+        addItem(new DummyItem("15", "D7", 0xD4)); // 0212
+        addItem(new DummyItem("16", "D#/Eb", 1343));
+        addItem(new DummyItem("17", "Ebm", 1342));
+        addItem(new DummyItem("18", "E", 2100));
+        addItem(new DummyItem("19", "Em", 2000));
+        addItem(new DummyItem("20", "F", 3211));
+        addItem(new DummyItem("21", "Fm", 3111));
+        addItem(new DummyItem("22", "F#/Gb", 4322));
+        addItem(new DummyItem("23", "G", 0x0003));
+        addItem(new DummyItem("24", "Gm", 0x14D)); // 0333
+        addItem(new DummyItem("25", "G#/Ab", 1114));
     }
 
     private static void addItem(DummyItem item) {
