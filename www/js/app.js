54b38777ff6e1f09134cda968d319c742b431643
nateperry <thenateperry@gmail.com>
2016-03-10 00:11:09 -0500

    added some more styles and second instrument

--- www/js/app.js
+++ www/js/app.js
@@ -102,6 +102,21 @@ module.exports = {
       'F': [3, 2, 1, 1],
       'G': [null, null, null, 3]
     }
+  },
+  'ukulele-concert': {
+    name: 'Concert Ukulele',
+    tuning: {
+      default: ['G', 'C', 'E', 'A']
+    },
+    chords: {
+      'A': [2, 1, null, null],
+      'B': [4, 3, 2, 2],
+      'C': [null, null, null, 3],
+      'D': [3, 3, 3, 3],
+      'E': [1, 4, null, 2],
+      'F': [2, null, 1, null],
+      'G': [null, 2, 3, 2]
+    }
   }
 };
 },{}]},{},[1]);
